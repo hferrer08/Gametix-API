7a17d71b87846307fcb9ddffc573b950d38c8b8f
hferrer08 <hubert.ferrerg8@gmail.com>
2026-02-02 23:12:42 -0300

    Primer commit API Gametix, probado en api.php /ping

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/ping', function () {
+    return response()->json([
+        'ok' => true,
+        'message' => 'GAMETIX API OK'
+    ]);
+});
