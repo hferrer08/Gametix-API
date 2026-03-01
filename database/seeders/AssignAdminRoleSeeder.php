e5c4e3a9922d559acdf5ed462386f0f06cc84303
hferrer08 <hubert.ferrerg8@gmail.com>
2026-03-01 16:28:19 -0300

    Asignar role seeder

--- database/seeders/AssignAdminRoleSeeder.php
+++ database/seeders/AssignAdminRoleSeeder.php
@@ -11,7 +11,7 @@ class AssignAdminRoleSeeder extends Seeder
 {
     public function run(): void
     {
-        $adminUser = User::where('email', 'admin@admin.cl')->first();
+        $adminUser = User::where('email', 'admin@example.com')->first();
         $adminRole = Role::where('name', 'admin')->first();
 
         if ($adminUser && $adminRole) {
